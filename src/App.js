1363ae72c26bfb1786ffc9b244ca01d581b26cd0
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-05 14:32:12 -0500

    Header Component and CRWN logo svg

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import HomePage from './pages/Homepage/HomePage';
 import ShopPage from './pages/Shop/ShopPage';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
