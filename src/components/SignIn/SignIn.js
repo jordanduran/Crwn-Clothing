5cc9ae78eb7521894fadc1d7cc34296934525ecf
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-05 15:13:47 -0500

    FormInput component and styling

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './SignIn.styles.scss';
+import FormInput from '../FormInput/FormInput';
 
 class SignIn extends Component {
   constructor() {
@@ -30,23 +31,23 @@ class SignIn extends Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name='email'
             type='email'
             value={this.state.email}
-            onChange={this.handleChange}
+            label='email'
+            handleChange={this.handleChange}
             required
           />
-          <label>Email</label>
-          <input
+
+          <FormInput
             name='password'
             type='password'
             value={this.state.password}
-            onChange={this.handleChange}
+            label='password'
+            handleChange={this.handleChange}
             required
           />
-          <label>Password</label>
-
           <input type='submit' value='Submit Form' />
         </form>
       </div>
